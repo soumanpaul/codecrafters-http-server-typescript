ddc033372d5d648d6bda5a406581a0123fdc2b93
soumanpaul <souman1729@gmail.com>
2025-09-26 23:53:14 +0530

    added write

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,7 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
 });
 
 server.listen(4221, "localhost");
